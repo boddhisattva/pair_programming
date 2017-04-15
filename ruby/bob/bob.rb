c33118da899bd57fc98c517be75a21e699e1b726
Mohnish G J <mail4mohnish@gmail.com>
2017-04-15 12:45:17 +0530

    Fix test_only_numbers related test

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,6 +1,6 @@
 class Bob
   def hey(remark)
-    if remark.upcase == remark
+    if remark.upcase == remark && remark.downcase != remark
       "Whoa, chill out!"
     elsif remark.chars.last == "?"
       "Sure."
